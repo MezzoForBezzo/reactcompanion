e9704525f513a52ac5cd1d3fc3860850f7090ced
Michael Berry <michael.berry@fluxfederation.com>
2018-10-09 17:18:46 +1300

    WIP

--- reactcompanion/Signup.js
+++ reactcompanion/Signup.js
@@ -1,60 +1,57 @@
-// SignUp.js
-import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import React from 'react';
+import { Button, Alert, Image, Icon, View, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+import { StackNavigator, DrawerItems, SafeAreaView, createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import LinearGradient from "react-native-linear-gradient";
+import {Container, Content, Header, Body, Title} from 'native-base';
+
+import CustomHeader from './CustomHeader';
 
 export default class SignUp extends React.Component {
-  state = { email: '', password: '', errorMessage: null }
-
-handleSignUp = () => {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(this.state.email, this.state.password)
-    .then(() => this.props.navigation.navigate('Main'))
-    .catch(error => this.setState({ errorMessage: error.message }))
+  static navigationOptions = {
+    drawerLabel: 'SignUp',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('./companion.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Login',
+      description: 'This is the SignUp page'
+  };
 }
+
 render() {
-    return (
-      <View style={styles.container}>
-        <Text>Sign Up</Text>
-        {this.state.errorMessage &&
-          <Text style={{ color: 'red' }}>
-            {this.state.errorMessage}
-          </Text>}
-        <TextInput
-          placeholder="Email"
-          autoCapitalize="none"
-          style={styles.textInput}
-          onChangeText={email => this.setState({ email })}
-          value={this.state.email}
-        />
-        <TextInput
-          secureTextEntry
-          placeholder="Password"
-          autoCapitalize="none"
-          style={styles.textInput}
-          onChangeText={password => this.setState({ password })}
-          value={this.state.password}
-        />
-        <Button title="Sign Up" onPress={this.handleSignUp} />
-        <Button
-          title="Already have an account? Login"
-          onPress={() => this.props.navigation.navigate('Login')}
-        />
-      </View>
-    )
+  return (
+    <Container>
+      <CustomHeader title={this.state.title} link= {() => this.props.navigation.openDrawer()} description={this.state.description} />
+        <Content>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Button
+            onPress={() => this.props.navigation.goBack()}
+            title="Go back home"
+          />
+          <Button
+            title="Home"
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
+          <Button
+            title="Menu"
+            onPress={() => this.props.navigation.openDrawer()}
+          />
+        </View>
+        </Content>
+      </Container>
+    );
+
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  icon: {
+    width: 24,
+    height: 24,
   },
-  textInput: {
-    height: 40,
-    width: '90%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginTop: 8
-  }
-})
+});
